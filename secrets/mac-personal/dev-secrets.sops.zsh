0450305468d252dfffe81bce1605fa4a6d3c5fbc
Aziz Fikri Mahmudi <afikrim10@gmail.com>
2025-12-08 08:39:44 +0700

    update personal secrets

--- secrets/mac-personal/dev-secrets.sops.zsh
+++ secrets/mac-personal/dev-secrets.sops.zsh
@@ -1,13 +1,14 @@
-# Personal/dev sandbox secrets (encrypt with sops when populated).
-
-export MEKARI_GITHUB_TOKEN="__SET_ME__"
-export MEKARI_BRAVE_API_KEY="__SET_ME__"
-export MEKARI_TAVILY_API_KEY="__SET_ME__"
-export MEKARI_CONFLUENCE_URL="https://jurnal.atlassian.net"
-export MEKARI_CONFLUENCE_USERNAME="aziz.fikri@mekari.com"
-export MEKARI_CONFLUENCE_API_TOKEN="__SET_ME__"
-export MEKARI_JIRA_URL="https://jurnal.atlassian.net"
-export MEKARI_JIRA_USERNAME="aziz.fikri@mekari.com"
-export MEKARI_JIRA_API_TOKEN="__SET_ME__"
-export MEKARI_BITBUCKET_USERNAME="afikrim_mkr"
-export MEKARI_BITBUCKET_APP_PASSWORD="__SET_ME__"
+{
+	"data": "ENC[AES256_GCM,data:YQeEf/lhMKeMLMkI1e0lbTKxJOrbbHHZYUPnPIFIReJPgaV+SLFlq12TJ3+uZ7AaEqUfv+qY2FIb2PXev8DUEG6Q8a8yoMKiD52SDByiDhaBFQ/gemck9+c97R4l4eYMgJxS62m/Vjj0Y+Vcf2R/l67Z9CTi8aUbH8xOyq2osP5NF/hJM2RqAF5SExepgDjGB7/Mgm1JB77GwxNUtcArsdqQtGUGi1ylJHUAKao=,iv:m2s4tu2hm1v1cCVcrIRtOhOeEJnGXuxqipGjJ4L6RZE=,tag:k4OII4oCc3d4RZD1G2k1Lw==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1xewgjv9042k8d89pwnvgttthjjjkxq7vjqnww4yvp60jxyqkvajq8arseg",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSAxL2psUVl2c2p1WFlVZ2JC\ncWxMZVA1Z1MyY2tFZ01KU2V2K3NHMDEzWDBjCnR6bVJReklwbjBsckliUnFwK1Rp\nMmdYNy85L1VYNUE0b3dodWkyREN2UEEKLS0tIDZhYldGK2hrTnBLWDA0RlJheU5z\nM2FFVENjVnAxSXY3cVlsN2FER0JOM2MKSJB8eKN1ald3YrgrBC27WGTrKdK8iM9n\nJPjeiQYEqxqgmBJA234Ycd9WqYGSyrhtDrYvftQiz5pCFWjyLndhlQ==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2025-12-08T01:39:10Z",
+		"mac": "ENC[AES256_GCM,data:FUMyiQNK3RdV/1vT1SiX1H7ycCewUDJzN1Txf7reoEr9jVHxM99v3xmnIRE2oNrpW9Xc6lIAy+FngG72icJdkEiwFFI+QsJUyL6uq2mlpQxS3LfMv17yYf7ZoSgCt8m98iD3pPAxyhWgEcsfAhFWqjEqi+zGeVkouSTTmEj2L6Y=,iv:nJm+DOxKv4IFr9fVIbkWfMqIWHRa78LyZcVkjDakrTg=,tag:AXSBbO2LgqWlpYwfBwRjVA==,type:str]",
+		"version": "3.11.0"
+	}
+}
